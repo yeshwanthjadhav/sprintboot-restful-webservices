835a6b930bda8060fc69a27926c9167827adaf49
Yeshwanth Jadhav <yeshwanth.jadhav@gmail.com>
2018-07-04 23:24:07 -0700

    first commit

---
+++ src/main/java/com/yash/rest/webservices/sprintbootrestfulwebservices/SprintbootRestfulWebservicesApplication.java
@@ -0,0 +1,12 @@
+package com.yash.rest.webservices.sprintbootrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootRestfulWebservicesApplication.class, args);
+	}
+}
