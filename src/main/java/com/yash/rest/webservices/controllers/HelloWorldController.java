f06cbe011682314e4b997bc51aa764caf92519d8
Yeshwanth Jadhav <yeshwanth.jadhav@gmail.com>
2018-07-05 01:02:57 -0700

    Hello World Bean added, It will generate JSON message

--- src/main/java/com/yash/rest/webservices/controllers/HelloWorldController.java
+++ src/main/java/com/yash/rest/webservices/controllers/HelloWorldController.java
@@ -1,15 +1,23 @@
 package com.yash.rest.webservices.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.yash.rest.webservices.beans.HelloWorldBean;
 
 @RestController
 public class HelloWorldController {
 
 	//Display Hello World Message
-	@RequestMapping(method=RequestMethod.GET, path = "/hello-world")
+	//@RequestMapping(method=RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path = "/hello-world")
 	public String helloWorld(){
 		return "Hello World";
 	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean(){
+		return new HelloWorldBean("Hello World");
+	}
+	
 }
