945308425045ce11b5c70edc470b5c9bb41be132
Yeshwanth Jadhav <yeshwanth.jadhav@gmail.com>
2018-07-07 23:40:22 -0700

    Service with Path Variable

--- src/main/java/com/yash/rest/webservices/controllers/HelloWorldController.java
+++ src/main/java/com/yash/rest/webservices/controllers/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.yash.rest.webservices.controllers;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yash.rest.webservices.beans.HelloWorldBean;
@@ -18,6 +21,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean(){
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+		return new HelloWorldBean(String.format("Hello World, %s ", name));
 	}
 	
 }
