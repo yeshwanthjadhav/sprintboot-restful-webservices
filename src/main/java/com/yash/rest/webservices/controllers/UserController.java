277085305bf7a36f357aa6aa16ab05d68012be95
Yeshwanth Jadhav <yeshwanth.jadhav@gmail.com>
2018-07-24 23:50:44 -0700

    HATEOAS (HyperMedia As The Engine Of Application State) for RESTful services

--- src/main/java/com/yash/rest/webservices/controllers/UserController.java
+++ src/main/java/com/yash/rest/webservices/controllers/UserController.java
@@ -5,14 +5,17 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -36,12 +39,21 @@ public class UserController {
 	}
 	
 	@GetMapping("/user/{id}")
-	public User retrieveUser(@PathVariable int id){
+	public Resource<User> retrieveUser(@PathVariable int id){
 		User user = userDao.findOne(id);
 		if(user==null){
 			throw new UserNotFoundException("id-"+id);
 		}
-		return userDao.findOne(id);	
+		
+		//"all users", SERVER_PATH + "/users"
+		//retrieve all users
+		Resource<User> resource = new Resource<User>(user);
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).getAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		
+		//return userDao.findOne(id);	
+		
+		return resource;
 	}
 	
 	@PostMapping("/user")
