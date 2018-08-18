7121aab5c6f71c696e3298f5e708866304360862
Yeshwanth Jadhav <yeshwanth.jadhav@gmail.com>
2018-08-18 16:39:38 -0700

    Versioning RESTful services - Header and Content Negotiation Approach

--- src/main/java/com/yash/rest/webservices/versioning/PersonVersioningController.java
+++ src/main/java/com/yash/rest/webservices/versioning/PersonVersioningController.java
@@ -3,16 +3,69 @@ package com.yash.rest.webservices.versioning;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+### Versioning
+ - Media type versioning(a.k.a "content negotiation" or "accept header")
+	- GitHub
+ - (Custom) headers versioning
+	- Microsoft
+ - URI Versioning
+	- Twitter
+ - Request Parameter versioning
+	- Amazon
+	
+ - Factors
+	- URI Pollution
+	- Misuse of HTTP Header
+	- Caching
+	- Can we execute the request on the browser?
+	- API Documentation
+ - No Perfect solution
+ */
+
 @RestController
 public class PersonVersioningController {
 
+	/** URI Versioning **/
 	@GetMapping("v1/person")
-	public PersonV1 personv1(){
+	public PersonV1 personV1(){
 		return new PersonV1("Yash");
 	}
 	
 	@GetMapping("v2/person")
-	public PersonV2 personv2(){
+	public PersonV2 personV2(){
+		return new PersonV2(new Name("Yash", "Jadhav"));
+	}
+	
+	/** Param Versioning **/
+	@GetMapping(value = "/person/param", params = "version=1")
+	public PersonV1 paramV1(){
+		return new PersonV1("Yash");
+	}	
+	@GetMapping(value = "/person/param", params = "version=2")
+	public PersonV2 paramV2(){
+		return new PersonV2(new Name("Yash", "Jadhav"));
+	}
+	
+	/** Header Versioning **/
+	@GetMapping(value = "/person/param", headers = "X-API-VERSION=1")
+	public PersonV1 headerV1(){
+		return new PersonV1("Yash");
+	}	
+	@GetMapping(value = "/person/param", headers = "X-API-VERSION=2")
+	public PersonV2 headerV2(){
+		return new PersonV2(new Name("Yash", "Jadhav"));
+	}
+	
+	/** Produces Versioning (It is also called mime type versioning)
+	 * In headers add 'Accept = application/yash.company.app-v1+json'
+	 * **/	
+	@GetMapping(value = "/person/produces", produces = "application/yash.company.app-v1+json")
+	public PersonV1 producesV1(){
+		return new PersonV1("Yash");
+	}	
+	@GetMapping(value = "/person/produces", produces = "application/yash.company.app-v2+json")
+	public PersonV2 producesV2(){
 		return new PersonV2(new Name("Yash", "Jadhav"));
 	}
 	
