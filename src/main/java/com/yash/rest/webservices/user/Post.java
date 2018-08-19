fc55d229ebd30f60ac3316feb9896eda94e4016a
Yeshwanth Jadhav <yeshwanth.jadhav@gmail.com>
2018-08-19 11:19:57 -0700

    Creating POST Entity and ManyToOne relationship with User Entity

---
+++ src/main/java/com/yash/rest/webservices/user/Post.java
@@ -0,0 +1,35 @@
+package com.yash.rest.webservices.user;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue
+	private String id;
+	private String description;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into user values(1, sysdate(), 'Yash');
 insert into user values(2, sysdate(), 'Isha');
 insert into user values(3, sysdate(), 'Ekansh');
+insert into post values(11, 'My First Post', 1);
+insert into post values(12, 'My Second Post', 1);
