64d28b21924ddd26869dd705c8afcf58fe1be520
Yeshwanth Jadhav <yeshwanth.jadhav@gmail.com>
2018-08-18 18:02:04 -0700

    Create User Entity and test data in H2 database

--- src/main/java/com/yash/rest/webservices/user/User.java
+++ src/main/java/com/yash/rest/webservices/user/User.java
@@ -2,6 +2,9 @@ package com.yash.rest.webservices.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -9,8 +12,11 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description = "All details about the user.")
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min = 2, message = "Name should have atleast 2 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'Yash');
+insert into user values(2, sysdate(), 'Isha');
+insert into user values(3, sysdate(), 'Ekansh');
